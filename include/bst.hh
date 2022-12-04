212d07d0a64dc6a11e906c65b53be6cd930c6db6
Prashant Kumar <pkumar14@stevens.edu>
2022-12-03 22:39:52 -0500

    Small changes to correct insertion

--- include/bst.hh
+++ include/bst.hh
@@ -1,178 +1,155 @@
-/* Source code for b-tree found at https://www.geeksforgeeks.org/introduction-of-b-tree-2/
- * Code has been adapted and modifed for the use of our chain project
+/* Source code for b-tree found at
+ * https://www.geeksforgeeks.org/introduction-of-b-tree-2/ Code has been adapted
+ * and modifed for the use of our chain project
  *
-*/
+ */
 
 #include "btTreeNode.hh"
 
 using namespace std;
 
-class Chain // Chain is implemented using a b-tree
+class Chain  // Chain is implemented using a b-tree
 {
-    private:
-        Node *root; 
-        int minDegree;
+ private:
+  Node *root;
+  int minDegree;
 
-    public:
+ public:
+  Chain(int minDegree) : root(nullptr), minDegree(minDegree){};
+  // TODO:: Chain(unsigned int, const string&)
+  // TODO:: Chain(unsighned int, const Rope&)
 
-        Chain(int minDegree) : root(nullptr), minDegree(minDegree){};
-        //TODO:: Chain(unsigned int, const string&)
-        //TODO:: Chain(unsighned int, const Rope&)
+  ~Chain(void){};  // TODO:; Implement destructor
 
-        ~Chain(void) {} ; //TODO:; Implement destructor
+  // CHAIN FUNCTIONS
+  // ******************************************************************/
 
-        // CHAIN FUNCTIONS ******************************************************************/
+  void printTree() { showTree(root); }
 
+  int getNumberStrings() {
+    return root->numKeys;  // TODO:: verify this
+  }
 
-        void printTree()
-        {
-            showTree(root);
-        }
+  /* TODO:: Since large files will need to take a page number input */
+  void showTree(Node *node) {
+    cout << "Tree" << endl;
+    if (node)  // if not nullptr
+    {
+      int index;
+      for (index = 1; index <= root->numKeys; index++) {
+        cout << "Value" << root->getKey(node, index) << endl;
+        if (root->childArr[index - 1]) showTree(node->childArr[index - 1]);
+      }
+      if (root->childArr[index]) showTree(node->childArr[index]);
+    }
+  }
 
-        int getNumberStrings() 
-        {
-            return root->numKeys; //TODO:: verify this
-        }
+  // Copes chain into new memory pool
+  void copy(char *, unsigned int) const;
 
-        /* TODO:: Since large files will need to take a page number input */
-        void showTree(Node *node)
-        {
-            cout << "Tree" << endl;
-            if(node) // if not nullptr
-            {
-                int index;
-                for(index = 1; index <= root->numKeys; index++)
-                {
-                    cout << "Value" << root->getKey(node, index) << endl;
-                    if(root->childArr[index - 1])
-                        showTree(node->childArr[index - 1]);
-                }
-                if(root->childArr[index])
-                    showTree(node->childArr[index]);
-            }
-        }
+  // Compresses chain into 1 string
+  void consolidate(void);
 
-        // Copes chain into new memory pool
-        void copy(char*, unsigned int) const;
+  // B-tree Functions based from
+  // https://www.geeksforgeeks.org/introduction-of-b-tree-2 *********
 
-        // Compresses chain into 1 string
-        void consolidate(void);
+  Node *searchTree(
+      int lineNumber)  // TODO:: Need to figure out what we search by
+  {
+    if (nullptr == root) {
+      return nullptr;  // Key was not found
+    } else {
+      root->searchTree(lineNumber);  // Keep searching
+    }
+  }
 
-        // B-tree Functions based from https://www.geeksforgeeks.org/introduction-of-b-tree-2 *********
+  /* All this function is dooing is going from node to node until reaching the
+   * end of tree */
+  void traverseTree() {
+    if (nullptr != root) {
+      root->traverseTree();
+    }
+  }
 
-        Node *searchTree(int lineNumber) // TODO:: Need to figure out what we search by
-        {
-            if(nullptr == root) 
-            {
-                return nullptr;   //Key was not found
-            }
-            else
-            {
-                root->searchTree(lineNumber); // Keep searching
-            }
-        }
+  // TODO: implmenet insert for string type
+  /* void insert(int lineNumber, const string&)
+  {
+      if(nullptr == root)
+      {
+          int offset = 0;
+          char * line = "hello";
+          int index = 0;
+          root = new Node(minDegree, true);
+          root->setLineKey(root, index, lineNumber, offset, string);
+          //root->keys[0].key = lineNumber;
+          root->numKeys = 1;
+      }
+      else
+      {
+          if(root->numKeys == 2*minDegree - 1)
+          {
+              Node *newRoot = new Node(minDegree, false);
+              newRoot->childArr[0] = root;
+              newRoot->splitChild(0, root);
 
+              int index = 0;
+              if(newRoot->getKey(newRoot, index) < lineNumber) index++;
 
-        /* All this function is dooing is going from node to node until reaching the end of tree */
-        void traverseTree()
-        {
-            if( nullptr != root) { root->traverseTree();}
-        }
+              newRoot->childArr[index]->insertNonFull(lineNumber);
 
+              root = newRoot;
+          }
+          else
+          {
+              root->insertNonFull(lineNumber);
+          }
+      }
+  } */
 
-        //TODO: implmenet insert for string type
-        /* void insert(int lineNumber, const string&)
-        {
-            if(nullptr == root)
-            {
-                int offset = 0;
-                char * line = "hello";
-                int index = 0;
-                root = new Node(minDegree, true);
-                root->setLineKey(root, index, lineNumber, offset, string);
-                //root->keys[0].key = lineNumber;
-                root->numKeys = 1;
-            }
-            else
-            {
-                if(root->numKeys == 2*minDegree - 1)
-                {
-                    Node *newRoot = new Node(minDegree, false);
-                    newRoot->childArr[0] = root;
-                    newRoot->splitChild(0, root);
+  void insert(int lineNumber, const char *line) {
+    if (nullptr == root) {
+      int offset = 0;
+      int index = 0;
+      root = new Node(minDegree, true);
+      // root->keys[index].key = lineNumber;
+      // root->keys[index].line = lineNumber;
+      root->setLineKey(root, index, lineNumber, offset, line);
+      root->numKeys = 1;
+    } else {
+      if (root->numKeys == 2 * minDegree - 1) {
+        Node *newRoot = new Node(minDegree, false);
+        newRoot->childArr[0] = root;
+        newRoot->splitChild(0, root);
 
-                    int index = 0;
-                    if(newRoot->getKey(newRoot, index) < lineNumber) index++;
+        int index = 0;
+        if (newRoot->getKey(newRoot, index) < lineNumber) index++;
 
-                    newRoot->childArr[index]->insertNonFull(lineNumber);
+        newRoot->childArr[index]->insertNonFull(lineNumber, line);
 
-                    root = newRoot;
-                }
-                else
-                {
-                    root->insertNonFull(lineNumber);
-                }
-            }
-        } */
+        root = newRoot;
+      } else {
+        root->insertNonFull(lineNumber, line);
+      }
+    }
+  }
 
-        void insert(int lineNumber, const char* line)
-        {
-            if(nullptr == root)
-            {
-                int offset = 0;
-                int index = 0;
-                root = new Node(minDegree, true);
-                root->setLineKey(root, index, lineNumber, offset, line);
-                root->numKeys = 1;
-            }
-            else
-            {
-                if(root->numKeys == 2*minDegree - 1)
-                {
-                    Node *newRoot = new Node(minDegree, false);
-                    newRoot->childArr[0] = root;
-                    newRoot->splitChild(0, root);
+  // Remove based on key
+  void remove(int lineNumber) {
+    if (!root) {
+      return;
+    }
 
-                    int index = 0;
-                    if(newRoot->getKey(newRoot, index) < lineNumber) index++;
+    root->remove(lineNumber);
 
-                    newRoot->childArr[index]->insertNonFull(lineNumber, line);
+    if (root->numKeys == 0) {
+      Node *tmpNode = root;
+      if (root->isLeaf) {
+        root = nullptr;
+      } else {
+        root = root->childArr[0];
+      }
 
-                    root = newRoot;
-                }
-                else
-                {
-                    root->insertNonFull(lineNumber, line);
-                }
-            }
-        }
-
-        // Remove based on key
-        void remove(int lineNumber)
-        {
-            if(!root)
-            {
-                return;
-            }
-
-            root->remove(lineNumber);
-
-            if(root->numKeys == 0)
-            {
-                Node *tmpNode = root;
-                if(root->isLeaf)
-                {
-                    root = nullptr;
-                }
-                else
-                {
-                    root = root->childArr[0];
-                }
-
-                delete tmpNode;
-            }
-        }
+      delete tmpNode;
+    }
+  }
 };
-        
-
-
