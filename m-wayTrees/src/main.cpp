6493def79193b04b9ff895567d159a05e8e05f05
Prashant Kumar <pkumar14@stevens.edu>
2022-12-10 19:29:57 -0500

    insertion at pos 1

--- m-wayTrees/src/main.cpp
+++ m-wayTrees/src/main.cpp
@@ -16,37 +16,60 @@ class Chain {
   class internalNode {
     leafNode* nextLeaf[M];
     internalNode* nextNode[M];
-    uint32_t count[M];
+    uint32_t count[M] = {0};
     bool pointLeaf;
     // int flag;
     friend class Chain;
   };
-
-  internalNode* root;
+  class rootNode {
+    internalNode* nextNode[M];
+    uint32_t count[M] = {0};
+    friend class Chain;
+  };
+  rootNode* root;
   // leafNode
 
  public:
-  Chain() {
-    for (int i = 0; i < M; i++) {
-      root->nextLeaf[i] = nullptr;
-      root->nextNode[i] = nullptr;
-      root->count[i] = 0;
-      root->pointLeaf = true;
+  Chain() : root(nullptr) {}
+
+  void setLeaf(string line, int pos, leafNode* leaf) {
+    leaf->isleaf = true;
+    leaf->lines[pos] = line;
+  }
+
+  void setInternalNodePointingNode(internalNode* inode) {
+    if (inode->nextNode != nullptr) {
+      inode->pointLeaf = false;
+    }
+  }
+  void setInternalNodePointingLeaf(internalNode* inode) {
+    if (inode->nextNode == nullptr) {
+      inode->pointLeaf = true;
     }
   }
 
   void insertEnd(string line) {
+    // internalNode* temp = new internalNode;
     if (root == nullptr) {
+      rootNode* temp = new rootNode();
+      internalNode* inode = new internalNode();
       leafNode* leaf = new leafNode();
-      leaf->isleaf = true;
-      leaf->lines[0] = line;
-      root->nextNode[0] = nullptr;
-      root->nextLeaf[0] = leaf;
-      root->count[0] += 1;
+      setLeaf(line, 0, leaf);
+      // leaf->isleaf = true;
+      // leaf->lines[0] = line;
+      temp->nextNode[0] = inode;
+      inode->nextLeaf[0] = leaf;
+      inode->pointLeaf = true;
+      temp->count[0] += 1;
+      inode->count[0] += 1;
+      root = temp;
     } else {
-      for (int i = 0; i < M; i++) {
+      // to get hold of the last available block
+      for (int i = M - 1; i >= 0; i--) {
         while (root->nextNode[i] != nullptr)
           if (root->count[i] < M) {
+            for (int j = 0; j < M; j++) {
+            }
           }
       }
     }
@@ -58,6 +81,8 @@ int main() {
   string str = "Hello, World!";
   string other = "Welcome aboard:";
   string another = "Lets go to LaLaLand.";
+  string yetanother = "Lets go to LaLaLand, again";
+
   c.insertEnd(str);
   return 0;
 }
