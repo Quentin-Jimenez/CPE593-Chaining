6d040a951f688d6696caf4721c784122be117722
Noah Malhi <noahmalhi.6624@gmail.com>
2022-12-11 11:46:48 -0500

    Working on split function for adding to end

--- m-wayTrees/src/test.cpp
+++ m-wayTrees/src/test.cpp
@@ -32,55 +32,7 @@ class Chain
     Chain() : root(nullptr) {};  //Constructor
     ~Chain() {};                //Destructor
 
-    // Searches value in the node
-    /*struct node* search(int val, InternalNode *root, int* pos)
-    {
-
-        // if root is Null then return
-        if (root == NULL)
-            return NULL;
-        else {
-
-            // if node is found
-            if (searchnode(val, root, pos))
-                return root;
-
-            // if not then search in child nodes
-            else
-                return search(val,
-                        root->child[*pos],
-                        pos);
-        }
-    }
-
-    // Searches the node
-    int searchnode(int val,
-            struct node* n,
-            int* pos)
-    {
-        // if val is less than node->value[1]
-        if (val < n->value[1]) {
-            *pos = 0;
-            return 0;
-        }
-
-        // if the val is greater
-        else {
-            *pos = n->count;
-
-            // check in the child array
-            // for correct position
-            while ((val < n->value[*pos])
-                    && *pos > 1)
-                (*pos)--;
-            if (val == n->value[*pos])
-                return 1;
-            else
-                return 0;
-        }
-    }
-    */
-
+    
     // Inserts a value in the m-Way tree
     void insertEnd(string line)
     {
@@ -92,11 +44,11 @@ class Chain
         // returns a value 0 if the new value
         // is inserted in the tree, otherwise
         // it returns a value 1
-        flag = setval(line, root, &i, &child);
+        setval(line, root, &i, &child);
     }
 
     // Sets the value in the node
-    int setval(string line, InternalNode* node, string *p, InternalNode **child)
+    void setval(string line, InternalNode* node, string *p, InternalNode **child)
     {
         int k = 0;
 
@@ -104,7 +56,7 @@ class Chain
         if (node == NULL) {
             *p = line;
             *child = NULL;
-            return 1;
+            return;
         }
         else {
 
@@ -118,15 +70,14 @@ class Chain
                     if(node->count[index] < M)
                     {
                         fillnode(line, node->nextLeaf[index]);
-                        return 1;
-                        
+                        return;
                     }
                 }
 
                 if(isNodeFull)
                 {
-                    //split(*p, *child, node, k, p, child);
-                    return 0;
+                    splitEnd(line, node);
+                    return;
                 }
                 
             }
@@ -142,7 +93,7 @@ class Chain
                 }
             }
         }
-        return 0;
+        return;
     }
 
     // Adjusts the value of the node
@@ -156,44 +107,28 @@ class Chain
     }
 
     // Splits the node
-    /*void split(int val,
-            struct node* c,
-            struct node* n,
-            int k, int* y,
-            struct node** newnode)
+    void splitEnd(string line, InternalNode *head)
     {
-        int i, mid;
-        if (k <= MIN)
-            mid = MIN;
-        else
-            mid = MIN + 1;
+        
+        InternalNode *newNodeLeft = new InternalNode();
+        InternalNode *newNodeRight = new InternalNode();
+        InternalNode *tempHeadNode = new InternalNode();
+        LeafNode *newLeaf = new LeafNode();
+    
+        //TODO hardcoded for M = 4. Will need to change this
+        newNodeLeft = head;
 
-        // Allocating the memory for a new node
-        *newnode = new node();
+        fillnode(line, newLeaf);
+        newNodeRight->nextLeaf[0] = newLeaf;
+        newNodeRight->isLeafNode = true;
+        newNodeRight->count[0] = 1;
 
-        for (i = mid + 1; i <= MAX; i++) {
-            (*newnode)->value[i - mid] = n->value[i];
-            (*newnode)->child[i - mid] = n->child[i];
-        }
+        tempHeadNode->isLeafNode = false;
+        tempHeadNode->nextNode[0] = newNodeLeft;
+        tempHeadNode->nextNode[1] = newNodeRight;
 
-        (*newnode)->count = MAX - mid;
-        n->count = mid;
-
-        // it checks whether the new value
-        // that is to be inserted is inserted
-        // at a position less than or equal
-        // to minimum values required in a node
-        if (k <= MIN)
-            fillnode(val, c, n, k);
-        else
-            fillnode(val, c, *newnode, k - mid);
-
-        *y = n->value[n->count];
-        (*newnode)->child[0] = n->child[n->count];
-        n->count--;
+        head = tempHeadNode;
     }
-    */
-
 };
 
 int main() {
