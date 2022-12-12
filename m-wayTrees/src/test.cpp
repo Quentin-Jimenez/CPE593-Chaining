1fc6a55021817cd97d008d67b77c0b4569874140
Noah Malhi <noahmalhi.6624@gmail.com>
2022-12-12 12:20:00 -0500

    Insert end working for up to 64 lines so far

--- m-wayTrees/src/test.cpp
+++ m-wayTrees/src/test.cpp
@@ -21,6 +21,7 @@ class Chain {
     int count[M] = {0};  // Tracks the number of leaves below
 
     InternalNode *nextNode[M];
+    InternalNode *parentNode;   //keeps track of previous node, used to update count
     LeafNode *nextLeaf[M];  // These are all null unless a leaf node
 
     friend class Chain;
@@ -33,270 +34,260 @@ class Chain {
   ~Chain(){};                 // Destructor
 
   void printTree() {
-    /*cout << root->nextLeaf[0]->lines[0];
-    cout << root->nextLeaf[0]->lines[1];
-    cout << root->nextLeaf[0]->lines[2];
-    cout << root->nextLeaf[0]->lines[3];
-    cout << endl;
-    cout << root->nextLeaf[1]->lines[0];
-    cout << root->nextLeaf[1]->lines[1];
-    cout << root->nextLeaf[1]->lines[2];
-    cout << root->nextLeaf[1]->lines[3];
-    cout << endl;
-    cout << root->nextLeaf[2]->lines[0];
-    cout << root->nextLeaf[2]->lines[1];
-    cout << root->nextLeaf[2]->lines[2];
-    cout << root->nextLeaf[2]->lines[3];
-    cout << endl;
-    cout << root->nextLeaf[3]->lines[0];
-    cout << root->nextLeaf[3]->lines[1];
-    cout << root->nextLeaf[3]->lines[2];
-    cout << root->nextLeaf[3]->lines[3];
-    cout << endl;
-    */
 
+      for(int q = 0; q < M; q++)
+        cout << root->count[q] << endl;
 
-     cout << "Node 1" << endl;
-     cout << "Leaf 1" << endl;
-     cout << root->nextNode[0]->nextLeaf[0]->lines[0];
-     cout << root->nextNode[0]->nextLeaf[0]->lines[1];
-     cout << root->nextNode[0]->nextLeaf[0]->lines[2];
-     cout << root->nextNode[0]->nextLeaf[0]->lines[3];
+      cout << "Leaf Count" << root->nextNode[1]->count[0] << endl;
+      cout << "Leaf Count" << root->nextNode[1]->count[1] << endl;
 
-     cout << endl;
-     cout << "Leaf2" << endl;
-     cout << root->nextNode[0]->nextLeaf[1]->lines[0];
-     cout << root->nextNode[0]->nextLeaf[1]->lines[1];
-     cout << root->nextNode[0]->nextLeaf[1]->lines[2];
-     cout << root->nextNode[0]->nextLeaf[1]->lines[3];
-
-     cout << endl;
-     cout << "leaf 3" << endl;
-     cout << root->nextNode[0]->nextLeaf[2]->lines[0];
-     cout << root->nextNode[0]->nextLeaf[2]->lines[1];
-     cout << root->nextNode[0]->nextLeaf[2]->lines[2];
-     cout << root->nextNode[0]->nextLeaf[2]->lines[3];
-     cout << endl;
-     cout << "Leaf 4" << endl;
-
-     cout << root->nextNode[0]->nextLeaf[3]->lines[0];
-     cout << root->nextNode[0]->nextLeaf[3]->lines[1];
-     cout << root->nextNode[0]->nextLeaf[3]->lines[2];
-     cout << root->nextNode[0]->nextLeaf[3]->lines[3];
-
-     cout <<endl;
-     cout << "Node 2" << endl;
-     cout << "Leaf 1" << endl;
-     cout << root->nextNode[1]->nextLeaf[0]->lines[0];
-
-  }
-
- 
-  // Sets the value in the node
-  void setval(string line, InternalNode *node, string *p,
-              InternalNode **child) {
-    int k = 0;
-
-    // if node is null
-    if (node == NULL) {
-      InternalNode *newRoot = new InternalNode();
-      LeafNode *leafNode = new LeafNode();
-      leafNode->lines[0] = line;
-      leafNode->count = 1;
-      newRoot->isLeafNode = true;
-      newRoot->count[0] = 1;
-      newRoot->nextLeaf[0] = leafNode;
-      root = newRoot;
-      return;
-    } else {
-      // Check if node points to leaf, if it doesn't recursively call and go to
-      // last possible node
-      if (node->isLeafNode) {
-        int isNodeFull = true;
-        for (int index = 0; index < M; index++) {
-          if (node->count[index] < M) {
-            if (node->nextLeaf[index] == nullptr) {
-              LeafNode *newLeaf = new LeafNode();
-              node->nextLeaf[index] = newLeaf;
-              node->count[index] = 1;
-              fillnode(line, node->nextLeaf[index], true);
-              return;
-            }
-
-            fillnode(line, node->nextLeaf[index], false);
-            node->count[index] += 1;
-            return;
+      
+      for(int k = 0; k < M; k++)
+      {
+          cout << "Node: " << k << endl;
+          cout << "Top node count " << root->count[k] << endl;
+          for(int m = 0; m < M; m++)
+          {
+              cout << "Leaf Node: " << m << endl;
+              cout << "Count " << root->nextNode[k]->count[m] << endl;
+              for(int i = 0; i < M; i++)
+              {
+                  cout << root->nextNode[k]->nextLeaf[m]->lines[i];
+              }
+              cout << endl;
           }
-        }
-
-        if (isNodeFull) {
-          cout << "In split" << endl;
-          splitEnd(line, node);
-          return;
-        }
-      } else {
-        for (int index = M - 1; index >= 0; index--) {
-          // Check for the last node that is populated and go to that node
-          if (node->count[index] > 0) {
-            setval(line, node->nextNode[index], p, child);
-          }
-        }
+          cout << endl;
       }
-    }
-    return;
+      
+
+      cout << endl;
   }
 
+  InternalNode * countBacktrace(InternalNode * node, int index)
+  {
+      int newCount = 0; 
+      int firstBack = true;
+
+      while(1)
+      {
+          if(node->parentNode == nullptr)
+          {
+              break;
+          }
+
+          
+          if(node->isLeafNode)
+          {
+              for(int i = 0; i < M; i++)
+              {
+                  if(node->nextLeaf[i] != nullptr)
+                  {
+                      newCount += node->nextLeaf[i]->count;
+                  }
+              }
+          }
+          else
+          {
+              if(firstBack != true)
+              {
+                index = 3;
+              }
+              else
+              {
+                  firstBack = false;
+              }
+
+              for(int i = 0; i < M; i++)
+              {
+                  newCount += node->count[i];
+              }
+          }
+
+          node = node->parentNode;
+          for(int j = index; j < M; j++)
+          {
+              cout << " J " << j << endl;
+              // Update the entire count array
+              if( j == 0)
+              {
+                  node->count[j] = newCount;
+                  break;
+              }
+
+              // Count[i] equals how many lines are in the node plus the previous nodes count
+              node->count[j] = newCount + node->count[j-1];
+
+              // Only need to update the next few nodes if they are not zero
+              if( j != 3 && node->count[j+1] == 0)
+              {
+                  break;
+              }
+          }
+          newCount = 0;
+      }
+
+      return node;
+  }
+ 
   // Adjusts the value of the node
-  void fillnode(string line, LeafNode *node, bool isNewLeaf) {
-    // Update counter to add a leaf and insert line
-    int leafIndex;
-    if (isNewLeaf) {
-      leafIndex = 0;
-    } else {
-      leafIndex = node->count;
-    }
+  LeafNode* fillnode(string line, LeafNode *node, bool isNewLeaf) {
+      // Update counter to add a leaf and insert line
+      int leafIndex;
+      if (isNewLeaf) {
+          leafIndex = 0;
+      } else {
+          leafIndex = node->count;
+      }
 
-    cout << leafIndex << endl;
+      node->lines[leafIndex] = line;
+      node->count += 1;
 
-    node->lines[leafIndex] = line;
-    node->count += 1;
+      return node;
   }
 
   void deleteLeafNodes(InternalNode *node) {
-    for (int i = 0; i < M; i++) {
-      delete[] node->nextLeaf[i];
-    }
+      for (int i = 0; i < M; i++) {
+          delete[] node->nextLeaf[i];
+      }
   }
 
+  // Inserts a value in the m-Way tree
+  void insertEnd(string line)
+  {
+      setval(line, root, nullptr, false, 0);
+  }
+
+  // Sets the value in the node
+  void setval(string line, InternalNode *node, InternalNode **prevNode, bool isPrevSet, int prevIndex) 
+  {
+
+      // if node is null
+      if (node == NULL) {
+          InternalNode *newRoot = new InternalNode();
+          LeafNode *leafNode = new LeafNode();
+          leafNode->lines[0] = line;
+          leafNode->count = 1;
+          newRoot->isLeafNode = true;
+          newRoot->count[0] = 1;
+          newRoot->parentNode = nullptr;
+          newRoot->nextLeaf[0] = leafNode;
+          root = newRoot;
+      }
+      else {
+
+          //Check if node points to leaf, if it doesn't recursively call and go to last possible node
+
+          if(!isPrevSet)
+          {
+              prevNode = &root;
+              isPrevSet = true;
+          }
+
+          if(node->isLeafNode)
+          {
+              int isNodeFull = true;
+              for(int index = 0; index < M; index++)
+              {
+                  if(node->count[index] < M)
+                  {
+
+                      cout << "Previous index" << prevIndex <<endl;
+                      if(node->nextLeaf[index] == nullptr)
+                      {
+                          LeafNode *newLeaf = new LeafNode();
+                          newLeaf = fillnode(line, newLeaf, true );
+                          node->nextLeaf[index] = newLeaf;
+                          node->count[index] = 1;
+                          countBacktrace(node, prevIndex);
+                          return;
+                      }
+
+                      node->nextLeaf[index] = fillnode(line, node->nextLeaf[index], false);
+                      countBacktrace(node, prevIndex);
+
+                      node->count[index] += 1;
+
+                      return;
+                  }
+              }
+
+              if(isNodeFull)
+              {
+                  splitEnd(line, &node);
+                  countBacktrace(node->nextNode[0], 0);
+                  countBacktrace(node->nextNode[1], 1);
+                  *prevNode = node;
+
+                  return;
+              }
+          }
+          else {
+
+              // keep track of last node
+              *prevNode = node;
+
+              for(int nodeIndex = 0; nodeIndex < M;  nodeIndex++)
+              {
+                  // Check for the last node that is populated and go to that node
+                  cout << "Node count check " << node->count[nodeIndex] << endl;
+                  if(node->count[nodeIndex] % 16 > 0 || node->count[nodeIndex] == 0)
+                  {
+                      //TODO may need to pass index to this
+                      cout << "Index is " << nodeIndex<< endl; 
+                      setval(line, node->nextNode[nodeIndex], prevNode, isPrevSet, nodeIndex);
+                      return;
+                  }
+              }
+
+              // Only reaches this point if all nodes are full. We then go down to next level from the last node
+              // This only works for insert end
+              setval(line, node->nextNode[3], prevNode, isPrevSet, 3);
+              return;
+
+          }
+      }
+  }
+
+
   // Splits the node
-  void splitEnd(string line, InternalNode *head) {
-    InternalNode *newNodeLeft = new InternalNode();
-    InternalNode *newNodeRight = new InternalNode();
-    InternalNode *tempHeadNode = new InternalNode();
-    LeafNode *newLeaf = new LeafNode();
+  void splitEnd(string line, InternalNode **head)
+  {
 
-    // TODO hardcoded for M = 4. Will need to change this
-    newNodeLeft = head;
+      InternalNode *newNodeOne = new InternalNode();
+      InternalNode *newNodeTwo = new InternalNode();
+      InternalNode *newNodeThree = new InternalNode();
+      InternalNode *newNodeFour = new InternalNode();
 
-        cout << root->nextNode[0]->nextLeaf[0]->lines[0];
 
-    }
-    
-    // Inserts a value in the m-Way tree
-    void insertEnd(string line)
-    {
-        setval(line, root, nullptr);
-    }
+      InternalNode *tempHeadNode = new InternalNode();
+      LeafNode *newLeaf = new LeafNode();
 
-    // Sets the value in the node
-    void setval(string line, InternalNode *node, InternalNode **prevNode) 
-    {
-        int k = 0;
+      //TODO hardcoded for M = 4. Will need to change this
+      newNodeOne = *head;
+      newNodeOne->isLeafNode = true;
 
-        // if node is null
-        if (node == NULL) {
-            InternalNode *newRoot = new InternalNode();
-            LeafNode *leafNode = new LeafNode();
-            leafNode->lines[0] = line;
-            leafNode->count = 1;
-            newRoot->isLeafNode = true;
-            newRoot->count[0] = 1;
-            newRoot->nextLeaf[0] = leafNode;
-            root = newRoot;
-        }
-        else {
+      fillnode(line, newLeaf, true);
+      newNodeTwo->nextLeaf[0] = newLeaf;
+      newNodeTwo->isLeafNode = true;
+      newNodeTwo->count[0] = 1;
+      newNodeTwo->parentNode = tempHeadNode;
 
-            //Check if node points to leaf, if it doesn't recursively call and go to last possible node
+      newNodeThree->isLeafNode = true;
+      newNodeThree->parentNode = tempHeadNode;
+      newNodeFour->isLeafNode = true;
+      newNodeFour->parentNode = tempHeadNode;
 
-            if(prevNode == nullptr)
-            {
-                prevNode = &root;
-            }
+      tempHeadNode->isLeafNode = false;
+      tempHeadNode->nextNode[0] = newNodeOne;
+      tempHeadNode->count[0] = 16;
+      tempHeadNode->nextNode[1] = newNodeTwo;
+      tempHeadNode->count[1] = 1;
+      tempHeadNode->nextNode[2] = newNodeThree;
+      tempHeadNode->nextNode[3] = newNodeFour;
 
-            InternalNode *tempNode = new InternalNode();
+      *head = tempHeadNode;
 
-            if(node->isLeafNode)
-            {
-                int isNodeFull = true;
-                for(int index = 0; index < M; index++)
-                {
-                    if(node->count[index] < M)
-                    {
-                        if(node->nextLeaf[index] == nullptr)
-                        {
-                            LeafNode *newLeaf = new LeafNode();
-                            node->nextLeaf[index] = newLeaf;
-                            node->count[index] = 1;
-                            fillnode(line, node->nextLeaf[index], true );
-                            return;
-                        }
-
-                        fillnode(line, node->nextLeaf[index], false);
-                        node->count[index] += 1;
-                        return;
-                    }
-                }
-
-                if(isNodeFull)
-                {
-                    splitEnd(line, &node);
-                    *prevNode = node;
-                    
-                    cout << "here"  << endl;
-                    cout << node->nextNode[1]->nextLeaf[0]->lines[0];
-                    cout << root->nextNode[1]->nextLeaf[0]->lines[0];
-                    cout << endl;
-                    cout << endl;
-                    return;
-                }
-            }
-            else {
-                
-                for(int index = M - 1; index >= 0;  index--)
-                {
-                    // Check for the last node that is populated and go to that node
-                    if(node->count[index] > 0)
-                    {
-                        prevNode = &node;
-                        setval(line, node->nextNode[index], prevNode);
-                    }
-                }
-            }
-        }
-        return;
-    }
-
-      
-    // Splits the node
-    void splitEnd(string line, InternalNode **head)
-    {
-        
-        InternalNode *newNodeOne = new InternalNode();
-        InternalNode *newNodeTwo = new InternalNode();
-        InternalNode *newNodeThree = new InternalNode();
-        InternalNode *newNodeFour = new InternalNode();
-
-            
-        InternalNode *tempHeadNode = new InternalNode();
-        LeafNode *newLeaf = new LeafNode();
-    
-        //TODO hardcoded for M = 4. Will need to change this
-        newNodeOne = *head;
-
-        fillnode(line, newLeaf, true);
-        newNodeTwo->nextLeaf[0] = newLeaf;
-        newNodeTwo->isLeafNode = true;
-        newNodeTwo->count[0] = 1;
-
-        tempHeadNode->isLeafNode = false;
-        tempHeadNode->nextNode[0] = newNodeOne;
-        tempHeadNode->nextNode[1] = newNodeTwo;
-        tempHeadNode->nextNode[2] = newNodeThree;
-        tempHeadNode->nextNode[3] = newNodeFour;
- 
-        *head = tempHeadNode;
-
-    }
+  }
 };
 
 int main() {
@@ -323,9 +314,21 @@ int main() {
     c.insertEnd(another);
     c.insertEnd(yetanother);
 
+    
     string thisNew = " Tryhing to split";
     c.insertEnd(thisNew);
+    string extrastring = "Please work";
+    c.insertEnd(extrastring);
+    c.insertEnd(str);
 
-  c.printTree();
-  return 0;
+    
+    for(int i = 0; i < 43; i++)
+    {
+        c.insertEnd(extrastring);
+    }
+    c.insertEnd(another);
+    c.insertEnd(yetanother);
+    
+    c.printTree();
+    return 0;
 }
